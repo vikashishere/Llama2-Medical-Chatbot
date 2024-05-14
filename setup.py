abb916df0101e4355e74721c325b3e5d3f74471c
Vikash <vikashdas770@gmail.com>
2024-05-14 11:13:07 +0530

    project structure ready, venv installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Vikash Das',
+    author_email= 'vikashdas770@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
