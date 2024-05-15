350c24698c2c2f0be860e42e94e381128b612cc1
Vikash <vikashdas770@gmail.com>
2024-05-15 08:58:48 +0530

    helper.py, store_index.py added

---
+++ src/helper.py
@@ -0,0 +1,47 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+import pandas as pd
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+#Extract data from the PDF
+def load_pdf(data):
+    logging.info(f"Loading PDF....")
+    loader = DirectoryLoader(data,
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    
+    documents = loader.load()
+    logging.info(f"PDF loaded")
+    return documents
+
+
+
+#Create text chunks
+def text_split(extracted_data):
+    logging.info(f"Creating text chunks...")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    logging.info(f"text chunks are prepared.")
+    return text_chunks
+
+
+
+#download embedding model
+def download_hugging_face_embeddings():
+    logging.info(f"Loading Embedding model....")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    logging.info(f"Embedding model loaded")
+    return embeddings
+
+
+
+#download embedding model
+def embedded_data(embeddings, text_chunks):
+    logging.info(f"Initiating embedding for text chunks, please wait...")
+    embeds = [embeddings.embed_query(t.page_content) for t in text_chunks]
+    df = pd.DataFrame({'id': map(str, range(1, len(embeds)+1)), 'vectors': embeds})
+    logging.info(f"Embedding completed for text chunks")
+    return df
---
+++ store_index.py
@@ -0,0 +1,60 @@
+from src.helper import *
+from langchain.vectorstores import Pinecone
+import pinecone
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv
+import os
+import random
+import itertools
+import pandas
+
+
+load_dotenv()
+API_KEY = os.getenv("PINECONE_API_KEY")
+
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+df = embedded_data(embeddings, text_chunks)
+
+# #Initializing the Pinecone
+pc = Pinecone(api_key=API_KEY)
+# index_name = "healthcare-chatbot2"
+index_name = "test-my-code"
+# print(index_name)
+
+# Verify the index exists
+indexes = pc.list_indexes()
+if index_name not in indexes.names():
+    # Create index if it doesn't exist (optional)
+    # logging.info(f"Create new index - {index_name}")
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric='cosine',
+        spec=pinecone.ServerlessSpec(
+            cloud='aws',
+            region='us-east-1'
+        )
+    )
+    logging.info(f"New index ready as {index_name}")
+
+# Connect to the index
+index = pc.Index(index_name)
+
+# Convert DataFrame to list of (id, vector) tuples
+data_tuples = list(df.itertuples(index=False, name=None))
+
+# Helper function to break an iterable into chunks of size batch_size
+def chunks(iterable, batch_size=100):
+    it = iter(iterable)
+    chunk = tuple(itertools.islice(it, batch_size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, batch_size))
+
+# Upsert data with 100 vectors per upsert request
+logging.info(f"Pushing embedded data to {index_name}")
+for ids_vectors_chunk in chunks(data_tuples, batch_size=100):
+    index.upsert(vectors=ids_vectors_chunk)
